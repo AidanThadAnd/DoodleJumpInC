4cdf57cd12b8a277c2d39ce2da17e8c685d462cd
Jackyw125 <133839896+Jackyw125@users.noreply.github.com>
2024-02-10 12:46:08 -0700

    Update RASTER.C

--- skeleton/RASTER.C
+++ skeleton/RASTER.C
@@ -1,6 +1,11 @@
 #include "raster.h"
 
-void plot_something_1(UINT16 *base, int x, int y, ...)
+void plot_bitmap_1(UINT16 *base, int x, int y, ...)
+{
+	/* [TO DO] delete this example function */
+}
+
+void plot_bitmap_2(UINT16 *base, int x, int y, ...)
 {
 	/* [TO DO] delete this example function */
 }
