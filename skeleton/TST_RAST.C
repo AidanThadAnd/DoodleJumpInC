dbb9a5bf09352afd1a7b4a56654b78d843f68ef2
Jackyw125 <133839896+Jackyw125@users.noreply.github.com>
2024-02-10 12:47:51 -0700

    Update TST_RAST.C

--- skeleton/TST_RAST.C
+++ skeleton/TST_RAST.C
@@ -5,8 +5,8 @@ int main()
 {
 	void *base = Physbase();
 
-	plot_something_1(base, 100, 100);
-	plot_something_2(base, 200, 200);
+	plot_bitmap_1(base, 100, 100);
+	plot_bitmap_2(base, 200, 200);
 
 	return 0;
 }
