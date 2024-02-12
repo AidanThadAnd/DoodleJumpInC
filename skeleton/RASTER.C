5765811670032eb0c7e2efe8fac3587916b975c8
AidanThadAnd <aidanandrews112@gmail.com>
2024-02-12 14:30:59 -0700

    plot_bitmap_16 works with coordinates

--- skeleton/RASTER.C
+++ skeleton/RASTER.C
@@ -13,11 +13,12 @@ void plot_bitmap_8(UINT16 *base, int x, int y, const UINT8 *bitmap, unsigned int
 
 void plot_bitmap_16(UINT16 *base, int x, int y, const UINT16 *bitmap, unsigned int height)
 {
-	int i=0;
-	
-	while(i<height)
-	{
-		*(base + i*40) = bitmap[i];
-		i++;
-	}
+    UINT16 *loc = base + (y * 40) + (x >> 4);
+    int row;
+    
+    for (row = 0; row < height; row++)
+    {
+        *loc |= bitmap[row];     /* danger (no bounds checking!) */
+        loc += 40;
+    }
 }
--- skeleton/TST_RAST.C
+++ skeleton/TST_RAST.C
@@ -2,11 +2,35 @@
 #include "raster.h"
 #include "font.h"
 
+#define SCREEN_HEIGHT 400
+#define SCREEN_WIDTH 640
+#define TEST_BITMAP_HEIGHT 13
+
 int main()
 {
 	UINT16 *base = Physbase();
 
+	int testBitmap[TEST_BITMAP_HEIGHT] =
+	{
+		0x0000,
+		0x0810,
+		0x0810,
+		0x0420,
+		0x0240,
+		0x1FF8,
+		0x2004,
+		0x4662,
+		0x4002,
+		0x43C2,
+		0x2424,
+		0x1008,
+		0x0FF0,
+	};
+
+	/*Plots "Space" in the top left corner*/
 	plot_bitmap_8(base, 0, 0, font, FONT_HEIGHT);
+
+	plot_bitmap_16(base, 320, 200, testBitmap, TEST_BITMAP_HEIGHT);
 
 	return 0;
 }
