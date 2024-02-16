640e4b791fde0eab533b4523eb1443e970ebbaee
Jackyw125 <133839896+Jackyw125@users.noreply.github.com>
2024-02-16 10:43:59 -0700

    Update MODEL.C, dont think any of this works. Haven't tested

--- skeleton/MODEL.C
+++ skeleton/MODEL.C
@@ -1,1 +1,24 @@
 #include "model.h"
+
+void move_doodle(Doodle *doodle)
+{
+    doodle->y += doodle->delta_y;
+    doodle->x += doodle->delta_x;
+}
+
+void move_platform(Platform *platform)
+{
+    pla->y += doodle->delta_y;
+    doodle->x += doodle->delta_x;
+}
+int check_collision_doodle_platform(Doodle *d, Platform *p)
+{
+    if (d->y + doodle_height > p->y &&
+        d->y < p->y + platform_height &&
+        d->x + doodle_width > p->x &&
+        d->x < p->x + platform_width)
+    {
+        return 1;
+    }
+    return 0;
+}
