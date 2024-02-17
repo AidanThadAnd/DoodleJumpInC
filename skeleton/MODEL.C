84c973a3e1594e68c42c8205ccc4e8548bbae7af
Jackyw125 <133839896+Jackyw125@users.noreply.github.com>
2024-02-16 20:23:52 -0700

    Update MODEL.C

--- skeleton/MODEL.C
+++ skeleton/MODEL.C
@@ -1,5 +1,14 @@
 #include "model.h"
 
+void initialize_model(Model *model) {
+    // Initialize Doodle character in the middle of the screen
+    model->doodle.x = SCREEN_WIDTH / 2;
+    model->doodle.y = SCREEN_HEIGHT / 2;
+    model->doodle.delta_x = 0;
+    model->doodle.delta_y = 0;
+    model->doodle.facing = 1; // Assuming initially facing right
+}
+
 void move_doodle(Doodle *doodle, int displacement_x, int displacement_y)
 {
     doodle->y += displacement_y;
