225270d01cf0d766fd3c2cf84f7b3650dc0aef75
Jackyw125 <133839896+Jackyw125@users.noreply.github.com>
2024-02-16 19:56:09 -0700

    Update EVENTS.C

--- skeleton/EVENTS.C
+++ skeleton/EVENTS.C
@@ -1,5 +1,22 @@
 #include "events.h"
 
+void press_key(Doodle *character, char key)
+{
+    switch(key)
+    {
+        case 'a':
+            move_doodle(character, -1, 0)
+            character->facing = 0;
+            break;
+        case 'd':
+            move_doodle(character, 1, 0)
+            character->facing = 1;
+            break;
+        default:
+            break:
+    }
+}
+
 int check_collision_doodle_platform(Doodle *d, Platform *p)
 {
     if (d->y + doodle_height > p->y &&
