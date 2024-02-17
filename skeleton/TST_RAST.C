68735013f789ae72bff52fc44498cc6dece75d2a
Aidan <aidanandrews112@gmail.com>
2024-02-16 20:06:34 -0700

    added documentation

--- skeleton/TST_RAST.C
+++ skeleton/TST_RAST.C
@@ -2,10 +2,13 @@
 Author: Aidan & Jacky
 Date: Jan 29 2024
 
-Program purpose: Displays various bitmaps to the ATARI 68000 screen for testing purposes
+Purpose: 
+				- Displays various bitmaps to the ATARI 68000 screen for testing purposes
 
-Details:        - Each function will clear the screen after plotting the given bitmap
-				- 
+Function Details: 
+				- Each function will clear the screen after plotting the given bitmap
+				- The function "test_plot_bitmap_8" and "test_plot_bitmap_16" will plot the given bit map equally across the entire screen
+				- The function "test_plot_bitmap_32" will plot the bitmap horizontally 40 times across the screen, each plot will be cleared
 
 Warning:		- Bitmaps must come with the height defined
 				- No error checking on bitmaps, if incorrect height or incorrect size is given the tests will come out incorrect
@@ -29,7 +32,7 @@ void test_plot_bitmap_32(UINT32 *base, const UINT32 *bitmap, const unsigned int 
 int main()
 {
 	
-    UINT32 *base  = Physbase(); /* Multiple pointers are created for Physbase to prevent pointer conversion warnings during compile time */
+    UINT32 *base  = Physbase();
         
 	const UINT8* glyph_A = GLYPH_START('A');
 
@@ -44,6 +47,22 @@ int main()
 	return 0;
 }
 
+/*
+Function Name: test_plot_bitmap_8
+
+Purpose:
+		- Uses the plot_bitmap_8 function from RASTER.C to fill the screen with the supplied bitmap,
+		  after each element is plotted the screen is cleared
+
+Input:
+		- A pointer to frame buffer called "base"
+		- A pointer to a bitmap called "bitmap"
+		- A integer which represents the height of the bitmap
+
+Output:
+		- Displays the given bitmap in a grid formation to the screen
+		- Screen is cleared after function is complete
+*/
 
 void test_plot_bitmap_8(UINT16 *base, const UINT8 *bitmap, const unsigned int height)
 {
@@ -58,6 +77,22 @@ void test_plot_bitmap_8(UINT16 *base, const UINT8 *bitmap, const unsigned int he
 
 }
 
+/*
+Function Name: test_plot_bitmap_16
+
+Purpose:
+		- Uses the plot_bitmap_16 function from RASTER.C to fill the screen with the supplied bitmap,
+		  after each element is plotted the screen is cleared
+
+Input:
+		- A pointer to frame buffer called "base"
+		- A pointer to a bitmap called "bitmap"
+		- A integer which represents the height of the bitmap
+
+Output:
+		- Displays the given bitmap in a grid formation to the screen
+		- Screen is cleared after function is complete
+*/
 void test_plot_bitmap_16(UINT16 *base, const UINT16 *bitmap, const unsigned int height)
 {
 	int i;
@@ -71,12 +106,30 @@ void test_plot_bitmap_16(UINT16 *base, const UINT16 *bitmap, const unsigned int 
 	clear_screen((UINT8*)base, SCREEN_HEIGHT, SCREEN_WIDTH);
 }
 
+/*
+Function Name: test_plot_bitmap_32
+
+Purpose:
+		- Uses the plot_bitmap_32 function from RASTER.C to fill the screen with the supplied bitmap,
+		  after each element is plotted the screen is cleared
+
+Input:
+		- A pointer to frame buffer called "base"
+		- A pointer to a bitmap called "bitmap"
+		- A integer which represents the height of the bitmap
+
+Output:
+		- Displays the given bitmap in a grid formation to the screen
+		- Screen is cleared after function is complete
+*/
 void test_plot_bitmap_32(UINT32 *base, const UINT32 *bitmap, const unsigned int height)
 {
     int i;
-	for(i = 0; i < SCREEN_WIDTH; i+=16)
-        {
-		plot_bitmap_32(base, i, SCREEN_HEIGHT/2, bitmap, height);
-        clear_screen((UINT8*)base, SCREEN_HEIGHT, SCREEN_WIDTH);
-        }
+	int j;
+
+	for(i = 0; i < SCREEN_WIDTH; i+=64)
+		for(j = 0; j < SCREEN_HEIGHT; j+=64)
+			plot_bitmap_32(base, i, j, bitmap, height);
+    
+	clear_screen((UINT8*)base, SCREEN_HEIGHT, SCREEN_WIDTH);
 }
