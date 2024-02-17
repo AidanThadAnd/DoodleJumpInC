96e97de115b00050e1119a931e202f77ce478ab8
Jackyw125 <133839896+Jackyw125@users.noreply.github.com>
2024-02-16 23:43:47 -0700

    Update tst_mod.c

--- skeleton/tst_mod.c
+++ skeleton/tst_mod.c
@@ -1,11 +1,40 @@
-#include "MODEL.H"
-#include "EVENTS.H"
+#include <stdio.h>
+#include "model.h"
+#include "events.h"
+#include "system.h"
 
-int main()
-{
-    int mynum;
+int main() {
+    Model model;
 
+    initialize_model(&model);
+    printf("Model initialized.\n");
 
+    int initialDoodleX = model.doodle.x;
+    int initialPlatformX = model.platforms[0].x;
+
+    press_key(&model.doodle, 'a');
+
+    if (initialDoodleX != model.doodle.x) {
+        printf("Doodle has moved.\n");
+    } else {
+        printf("Doodle has not moved.\n");
+    }
+
+    move_platform(&model.platforms[0]);
+
+    if (initialPlatformX != model.platforms[0].x) {
+        printf("Platform has moved.\n");
+    } else {
+        printf("Platform has not moved.\n");
+    }
+
+    if (check_collision_doodle_platform(&model.doodle, &model.platforms[0])) {
+        printf("Collision: Doodle collided with a platform!\n");
+    }
+
+    if (check_collision_monster(&model.doodle, &model.monster[0])) {
+        printf("Collision: Doodle collided with a monster!\n");
+    }
 
     return 0;
 }
