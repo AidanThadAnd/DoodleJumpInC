b4468207dfcd90118231c8bad007eaf00d8cebc4
Jackyw125 <133839896+Jackyw125@users.noreply.github.com>
2024-02-16 20:12:11 -0700

    Update EVENTS.C

--- skeleton/EVENTS.C
+++ skeleton/EVENTS.C
@@ -16,13 +16,13 @@ void press_key(Doodle *character, char key)
             break:
     }
 }
-
+ 
+/*  Check if the bottom of the Doodle is below the top of the platform
+    and if the top of the Doodle is above the bottom of the platform
+    Check if the right side of the Doodle is to the right of the left side of the platform
+    and if the left side of the Doodle is to the left of the right side of the platform */
 int check_collision_doodle_platform(Doodle *d, Platform *p)
 {
-    /* Check if the bottom of the Doodle is below the top of the platform
-       and if the top of the Doodle is above the bottom of the platform
-       Check if the right side of the Doodle is to the right of the left side of the platform
-       and if the left side of the Doodle is to the left of the right side of the platform */
     if (d->y + doodle_height > p->y &&
         d->y < p->y + platform_height &&
         d->x + doodle_width > p->x && 
