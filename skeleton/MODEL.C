be6be70cc4e296b9a46c466f47b04aa7541b1ef1
Jackyw125 <133839896+Jackyw125@users.noreply.github.com>
2024-02-16 21:23:39 -0700

    Update MODEL.C

--- skeleton/MODEL.C
+++ skeleton/MODEL.C
@@ -1,12 +1,12 @@
 #include "model.h"
 
 void initialize_model(Model *model) {
-    // Initialize Doodle character
+    /* Initialize Doodle character */
     model->doodle.x = SCREEN_WIDTH / 2;
     model->doodle.y = SCREEN_HEIGHT / 2;
     model->doodle.delta_x = 0;
     model->doodle.delta_y = 0;
-    model->doodle.facing = 1; // Assuming initially facing right
+    model->doodle.facing = 1; /* Assuming initially facing right */
 
     // Initialize platforms
     for (int i = 0; i < 7; ++i) {
@@ -14,7 +14,7 @@ void initialize_model(Model *model) {
         model->platforms[i].y = SCREEN_HEIGHT - (i * 80);
     }
 
-    // Initialize monster
+    /* Initialize monster */
     model->monster.x = SCREEN_WIDTH / 2;
     model->monster.y = SCREEN_HEIGHT - 100;
 }
