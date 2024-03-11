1475538aae02866fd322dd2b6d1852ebb4dbda3f
Jackyw125 <133839896+Jackyw125@users.noreply.github.com>
2024-03-11 17:25:05 -0600

    Update tst_mod.c

--- skeleton/tst_mod.c
+++ skeleton/tst_mod.c
@@ -3,36 +3,49 @@
 #include "events.h"
 #include "system.h"
 
+void print_state(Model *gameModel) {
+    printf("Doodle - X: %d, Y: %d, Facing: %d\n", gameModel->doodle.x, 
+		gameModel->doodle.y, gameModel->doodle.facing);
+
+    for (int i = 0; i < 7; ++i) {
+        printf("Platform %d - X: %d, Y: %d\n", i + 1, gameModel->platforms[i].x, gameModel->platforms[i].y);
+    }
+
+    printf("Monster - X: %d, Y: %d\n", gameModel->monster.x, gameModel->monster.y);
+}
+
 int main() {
-    Model model;
+    Model gameModel;
+    initialize_model(&gameModel);
 
-    initialize_model(&model);
+    printf("Initial state:\n");
+    print_state(&gameModel);
 
-    int initialDoodleX = model.doodle.x;
-    int initialPlatformX = model.platforms[0].x;
+    printf("\nPerforming actions:\n");
 
-    press_key(&model.doodle, 'a');
 
-    if (initialDoodleX != model.doodle.x) {
-        printf("Doodle has moved.\n");
+    press_key(&gameModel.doodle, 'd');  
+    printf("After moving right:\n");
+    print_state(&gameModel);
+
+    press_key(&gameModel.doodle, 'a');  
+    printf("After moving left:\n");
+    print_state(&gameModel);
+
+    
+    press_key(&gameModel.doodle, 'd');  
+    printf("After moving right:\n");
+    print_state(&gameModel);
+
+    press_key(&gameModel.doodle, 'a');
+    printf("After moving left:\n");
+    print_state(&gameModel);
+
+    
+    if (check_collision_monster(&gameModel.doodle, &gameModel.monster)) {
+        printf("Collision with Monster detected!\n");
     } else {
-        printf("Doodle has not moved.\n");
-    }
-
-    move_platform(&model.platforms[0]);
-
-    if (initialPlatformX != model.platforms[0].x) {
-        printf("Platform has moved.\n");
-    } else {
-        printf("Platform has not moved.\n");
-    }
-
-    if (check_collision_doodle_platform(&model.doodle, &model.platforms[1])) {
-        printf("Collision: Doodle collided with a platform!\n");
-    }
-
-    if (check_collision_monster(&model.doodle, &model.monster[1])) {
-        printf("Collision: Doodle collided with a monster!\n");
+        printf("No collision with Monster.\n");
     }
 
     return 0;
