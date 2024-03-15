6ba7171482716d26a3043d68b48547ff2d274196
Jackyw125 <133839896+Jackyw125@users.noreply.github.com>
2024-03-14 19:33:25 -0600

    Update DOODLE.C

--- skeleton/DOODLE.C
+++ skeleton/DOODLE.C
@@ -4,23 +4,21 @@
 #include <stdio.h>
 #include <osbind.h>
 
-UINT32 get_time(); 
-void delay(int milliseconds); 
-void erase_doodle(const Doodle doodle, UINT32 *base); 
+UINT32 get_time();
+void delay(int milliseconds);
+void erase_doodle(const Doodle doodle, UINT32 *base);
+void move_doodle(Doodle *doodle, UINT16 displacement_x, UINT16 displacement_y);
 
 int main() {
-    Model *model = initialize_model(); 
-
+    Model *model = initialize_model();
     UINT32 *base = Physbase();   
-
-    char pressedKey = 0; 
-
+    char pressedKey = 0;
     render(model, base);  /* Render the initial state of the model */
 
     while (1) { /* Main game loop */
         if (Cconis()) { /* Check if keyboard input is available */
             char key = (char)Cnecin(); /* Read keyboard input */
-            if (key == 'a' || key == 'd') { 
+            if (key == 'a' || key == 'd') {
                 pressedKey = key;
             }
         } else {
@@ -30,30 +28,31 @@ int main() {
         switch (pressedKey) { /* Handle the pressed key */
             case 'a':
                 erase_doodle(model->doodle, base);  
-                model->doodle.x -= 1; /* Move the doodle left */
+                move_doodle(&(model->doodle), -25, 0); /* Move the doodle left */
                 render_doodle(&(model->doodle), base); 
                 break;
             case 'd':
                 erase_doodle(model->doodle, base);  
-                model->doodle.x += 1; /* Move the doodle right */
+                move_doodle(&(model->doodle), 25, 0); /* Move the doodle right */
                 render_doodle(&(model->doodle), base); 
                 break;
             default:
                 break;
         }
+
     }
 
     return 0;
 }
 
-void erase_doodle(const Doodle doodle, UINT32 *base) { 
-    plot_bitmap_32(base, doodle.x, doodle.y, clear_bitmap, DOODLE_HEIGHT); 
+void erase_doodle(const Doodle doodle, UINT32 *base) {
+    plot_bitmap_32(base, doodle.x, doodle.y, clear_bitmap, DOODLE_HEIGHT);
 }
 
-UINT32 get_time() { 
-    UINT32 time; 
+UINT32 get_time() {
+    UINT32 time;
     long old_ssp = Super(0); 
-    time = *((UINT32 *)0x462); 
+    time = *((UINT32 *)0x462);
     Super(old_ssp); 
-    return time; 
+    return time;
 }
