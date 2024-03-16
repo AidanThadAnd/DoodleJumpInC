9e4ce8ed431062f6f48b1789749ec1bb9851483a
Jackyw125 <133839896+Jackyw125@users.noreply.github.com>
2024-03-15 21:54:12 -0600

    Update MUSIC.C

--- skeleton/MUSIC.C
+++ skeleton/MUSIC.C
@@ -1,7 +1,18 @@
-#ifndef MUSIC_H
-#define MUSIC_H
+#include "music.h"
+#include "psg.h"
 
-void start_music();
-void update_music(unsigned int time_elapsed);
+#define MELODY_LENGTH 5
+int melody[MELODY_LENGTH] = {451,402,379,319,301};
 
-#endif
+int current_note_index = 0;
+unsigned int time_since_last_note = 0;
+
+void start_music() {
+    set_tone(0, melody[0]); 
+    enable_channel(0, 1, 0); 
+    set_volume(0,10);
+}
+
+void update_music(unsigned int time_elapsed) {
+
+    }
