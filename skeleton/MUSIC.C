423d81294ecf961065a3ce25cb0cf5c0ae0d5c1a
Jackyw125 <133839896+Jackyw125@users.noreply.github.com>
2024-04-07 12:59:26 -0600

    Update MUSIC.C

--- skeleton/MUSIC.C
+++ skeleton/MUSIC.C
@@ -1,18 +1,76 @@
 #include "music.h"
-#include "psg.h"
 
-#define MELODY_LENGTH 5
-int melody[MELODY_LENGTH] = {451,402,379,319,301};
+int curr_note_a = 0;
 
-int current_note_index = 0;
-unsigned int time_since_last_note = 0;
+const Note song[] = {
+    {D_SHARP, QUARTER_NOTE},
+    {D_SHARP, QUARTER_NOTE},
+    {F_FLAT, QUARTER_NOTE},
+    {D_SHARP, QUARTER_NOTE},
+    {F_FLAT, QUARTER_NOTE},
+    {C_SHARP, QUARTER_NOTE},
+    {D_SHARP, QUARTER_NOTE},
+    {F_FLAT, QUARTER_NOTE},
+    {A_SHARP, QUARTER_NOTE},
+    {C_SHARP, QUARTER_NOTE},
+    {G_SHARP, QUARTER_NOTE},
+    {F_FLAT, QUARTER_NOTE},
+    {A_SHARP, QUARTER_NOTE},
+    {G_SHARP, QUARTER_NOTE},
+    {A_SHARP, QUARTER_NOTE},
+};
 
-void start_music() {
-    set_tone(0, melody[0]); 
-    enable_channel(0, 1, 0); 
-    set_volume(0,10);
+int SONG_A = sizeof(song) / sizeof(song[0]);
+
+void start_music()
+{
+    long oldssp = Super(0);
+
+    write_psg(MIXER_REG, MIXER_NONE);   /*put mixer into default state*/
+    
+    /*first note of the song*/
+    set_tone(0, song[curr_note_a].tuning);
+     
+    enable_channel(0, 1, 0);
+
+    set_volume(0, 0x0C);
+   
+    Super(oldssp);
 }
 
-void update_music(unsigned int time_elapsed) {
-
+void update_music()
+{
+    int new_note_a = 0;
+    /*
+    Move to the next note on each channel where applicable
+    */
+    if(TIMER_MUSIC >= song[curr_note_a].duration) {
+        /*restart from the first note when the song finishes; otherwise, move on to the next note*/
+        curr_note_a = (curr_note_a >= SONG_A - 1) ? 0 : curr_note_a + 1;
+        TIMER_MUSIC = 0;
+        new_note_a = 1;
     }
+    
+    /*
+    get the tone of the current note on each channel
+    */
+    /*only use channel C for music if it is not currently being used for sound effects*/
+      set_tone(0, song[curr_note_a].tuning);
+    /*
+    only turn volume back on if a new note has begun
+    */
+    /*only use channel C for music if it is not currently being used for sound effects*/
+        if(new_note_a == 1){
+            enable_channel(0, 1, 0);
+    }
+}
+
+void stop_music(){
+	long old_ssp = Super(0);
+    
+	stop_sound();
+    
+	Super(old_ssp);
+}
+
+
