332b91813240a8560ae3eb8d93bb3a89b3f3b9c2
Jackyw125 <133839896+Jackyw125@users.noreply.github.com>
2024-04-07 12:52:07 -0600

    Create EFFECTS.C

---
+++ skeleton/EFFECTS.C
@@ -0,0 +1,183 @@
+#include "effects.h"
+
+int effects = 0;
+
+/***********************************************************************
+ * Name: doodle_lands
+ * 
+ * Purpose: sound effect is played when the doodle lands on the platform
+ * Details: This function uses a combination of noise and envelope control
+ *          to create a short ding sound
+ * 
+ * Inputs: None
+ * Outputs: N/A
+ * Returns: None
+ ***********************************************************************/
+void doodle_lands() 
+{
+    int oldIpl;
+    long oldSsp;
+
+    /*interrupt masking*/
+    oldSsp = Super(0);
+    oldIpl = set_ipl(7);
+    Super(oldSsp);
+
+    oldSsp = Super(0);
+
+    set_noise(0x0F);
+
+    write_psg(MIXER_REG, MIXER_NONE);
+
+    enable_channel(1, 0, 1);
+    enable_channel(2, 0, 1);
+
+    set_envelope(0, 0x1000);
+    
+    TIMER_SNDFX = 0;    /*start timer*/
+    effects = 1;
+
+    Super(oldSsp);
+
+    oldSsp = Super(0);
+    set_ipl(oldIpl);
+    Super(oldSsp);
+}
+
+/***********************************************************************
+ * Name: platform_breaks
+ * 
+ * Purpose: sound effect is played when doodle lands on broken platform
+ * 
+ * Details: This function uses a combination of noise, tone, and 
+ *          envelope control to create a short breaking sound
+ * 
+ * Inputs: None
+ * Outputs: N/A
+ * Returns: None
+ ***********************************************************************/
+void platform_breaks() 
+{
+    int oldIpl;
+    long oldSsp;
+
+    /*interrupt masking*/
+    oldSsp = Super(0);
+    oldIpl = set_ipl(7);
+    Super(oldSsp);
+
+    oldSsp = Super(0);
+
+    set_tone(2, 0x0AA5);
+    set_noise(0x0F);
+
+    write_psg(MIXER_REG, MIXER_NONE);
+
+    enable_channel(0, 1, 1);
+    enable_channel(1, 1, 1);
+    enable_channel(2, 1, 1);
+
+    set_envelope(0, 0x3800);
+    
+    TIMER_SNDFX = 0;    /*start timer*/
+    effects = 1;
+
+    Super(oldSsp);
+
+    oldSsp = Super(0);
+    set_ipl(oldIpl);
+    Super(oldSsp);
+}
+
+/***********************************************************************
+ * Name: monster_near
+ * 
+ * Purpose: Sound effect for a monster attack, creating a buzz noise.
+ * 
+ * Details: This function utilizes the noise generator of the PSG to create
+ *          a buzzing sound effect resembling a monster nearby.
+ * 
+ * Inputs: None
+ * Outputs: N/A
+ * Returns: None
+ ***********************************************************************/
+
+void monster_near() 
+{
+    int oldIpl;
+    long oldSsp;
+
+    oldSsp = Super(0);
+    oldIpl = set_ipl(7);
+    Super(oldSsp);
+
+    oldSsp = Super(0);
+
+    set_tone(1, 4)
+    set_noise(0x0F);
+
+    enable_channel(1, 1, 1);
+    enable_channel(2, 1, 1);
+
+    write_psg(MIXER_REG, MIXER_NONE);
+
+    set_envelope(0x0, 0x2000);
+
+    TIMER_SNDFX = 0;
+    effects = 1;
+
+    Super(oldSsp);
+
+    oldSsp = Super(0);
+    set_ipl(oldIpl);
+    Super(oldSsp);
+}
+
+/***********************************************************************
+ * Name: updateSndfx
+ * 
+ * Purpose: Syncronously updates sound effects
+ * 
+ * Details: A timer to determine when a sound effects is completed
+ * 
+ * Inputs: None
+ * Outputs: N/A
+ *
+ * Returns: A boolean indicating whether or not a sound effect is
+ *          currently playing
+ ***********************************************************************/
+int update_sndfx()
+{
+    if(TIMER_SNDFX >= 42 && effects == 1){
+        stop_effects();
+        effects = 0;
+    }
+
+    return effects;
+}
+
+/***********************************************************************
+ * Name: stop_effects
+ * 
+ * Purpose: clears psg registers to stop effects
+ * 
+ * Details: all sound effects are disabled
+ *
+ * Inputs: None
+ * Outputs: N/A
+ * Returns: None
+ ***********************************************************************/
+void stop_effects()
+{
+    /*disable b&c in mixer*/
+    set_volume(1, 0);
+    set_volume(2, 0);
+
+    /*disable noise bits*/
+    write_psg(NOISE_FREQ_REG, 0x00);
+
+    /*disable envelope bits remove this if envelopes are used for music too*/
+    write_psg(ENV_FINE, 0x00);
+    write_psg(ENV_ROUGH, 0x00);
+    write_psg(ENV_SHAPE_CONTROL, 0x00);
+}
