78f928d2d0169b7324ab8ddd8b5ae4b8fe8ccbfc
Jackyw125 <133839896+Jackyw125@users.noreply.github.com>
2024-04-07 15:58:08 -0600

    Create VBL.C

---
+++ skeleton/VBL.C
@@ -0,0 +1,48 @@
+#include "VBL.H"
+
+/*init timers*/
+int TIMER_MUSIC = 0;
+int TIMER_SNDFX = 0;
+int TIMER_LANDED = 0;
+
+int seconds;
+UINT32 ticks;
+int vbl_flag;
+
+void do_vbl_isr()
+{
+    int sndfx_on;
+
+    g_ticks += 1;
+
+    /*increment timers*/
+    TIMER_MUSIC++;
+    TIMER_SNDFX++;
+
+    sndfx_on = update_sndfx();
+    update_music(sndfx_on);
+
+    /*increment seconds every 70 invocations of this function*/
+    if (ticks % 70 == 0)
+    {
+        seconds++;
+    }
+    vbl_flag = 1;
+}
+
+UINT32 getTime()
+{
+    return ticks;
+}
+
+void set_vbl_flag() {
+    vbl_flag = 0;
+}
+
+void set_ticks() {
+    ticks = 0;
+}
+
+void set_seconds() {
+    seconds = 0;
+}
